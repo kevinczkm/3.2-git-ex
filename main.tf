08a1814edcfed41a94aedf75fa258ceb903cf929
kevinczkm <kevinc.zkm@gmail.com>
2025-07-10 21:09:07 +0800

    added versions.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 1.9.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-
 provider "aws" {
   region = "ap-southeast-1"
 }
