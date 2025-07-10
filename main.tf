ff90cce23b4a25ed1c8035fd28f46d4ea85e814a
kevinczkm <kevinc.zkm@gmail.com>
2025-07-10 20:46:32 +0800

    updated errors

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "ap-southeast-1"
 }
@@ -5,10 +17,13 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
+    #checkov:skip=CKV_AWS_20:The bucket is a public static content host
     key    = "kvin"  #Change this
     region = "ap-southeast-1"
   }
 }
+
+
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix =  "kvin" # Set your bucket name here
